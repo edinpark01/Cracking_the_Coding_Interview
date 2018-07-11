b43168d0461ab97f87ec80664d1a38add76f5ae7
Braulio Tonaco <edu.tonaco@gmail.com>
2018-07-10 23:13:52 -0400

    First GitHub Commit!

--- Chapter 01 - Arrays and Strings/02 - ArrayList & Resizable Arrays/Concept.py
+++ Chapter 01 - Arrays and Strings/02 - ArrayList & Resizable Arrays/Concept.py
@@ -1,1 +1,19 @@
-print('test')
+"""
+In some languages, arrays (often called lists in this case) are automatically resizable.
+The array or list will grow as you append items. In other languages, like Java, arrays are fixed length.
+The size is defined when you create the array.
+
+When you need an array-like data structure that offers dynamic resizing, you would ussually use an ArrayList.
+An ArrayList is an array that resizes itself as needed while still providing O(1) access. A typical implementation
+that its amortized insertion time is still O(1).
+
+This is an essential data structure for interviews. Be sure you are confortable with dynamically resizable arrays/lists
+in whatever language you will be working with. NOTE: the same name of the data structure as well as the 'resizing factor'
+(which is 2) may vary.
+
+QUESTION:   Why is the amortized insertion runtime O(1)?
+ANSWER:     Suppose you have an array of size N. We can work backwards to compute how many elements we copied at each
+            capacity increase. Observe that when we increse the array to K elements, the array was previously
+            half that size. Therefore, we need to copy k/2 elements.
+"""
+
---
+++ Chapter 01 - Arrays and Strings/03 - StryingBuilder/Concept.py
@@ -0,0 +1,27 @@
+"""
+Imagine you are concatenating a list of strings, as shown below. What would the running time of this code be?
+For simplicity, assume that the strings are all the same length (call this x) and that there are n string.
+"""
+
+def joinWords(words):
+    sentence = ''
+
+    for word in words:
+        # len (word) = x
+        sentence = sentence + word
+
+    return sentence
+
+"""
+1st iteration: 0  + x = x  => a new string of size x is created and then each char is copied over
+2nd iteration: x  + x = 2x => a new string of size 2x is created and then each char is copied over
+3rd iteration: 2x + x = 3x => a new string of size 3x is created and then each char is copied over
+4th iteration: 3x + x = 4x => a new string of size 4x is created and then each char is copied over
+5th iteration: 5x + x = 5x => a new string of size 5x is created and then each char is copied over
+.
+.
+.
+nth iteration: nx + x = x(n + 1)
+
+O(x + 2x + 3x + 4x + 5x + ... + nx) = xn(n + 1) / 2 = O(n^2) 
+"""
