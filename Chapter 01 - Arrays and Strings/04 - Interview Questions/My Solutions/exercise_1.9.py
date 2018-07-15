412cc073eb72da7bf5cd931dd705133fa09591eb
Braulio Tonaco <edu.tonaco@gmail.com>
2018-07-15 13:11:42 -0400

    Added Explanation

--- Chapter 01 - Arrays and Strings/04 - Interview Questions/My Solutions/exercise_1.9.py
+++ Chapter 01 - Arrays and Strings/04 - Interview Questions/My Solutions/exercise_1.9.py
@@ -13,7 +13,7 @@ WATERBOTTLE is a rotation of ERBOTTLEWAT
 
 def isSubstring(w1, w2):
     if len(w1) == len(w2) and len(w1) > 0:
-        return True if w1 in (w2 + w2) else False
+        return True if w2 in (w1 + w1) else False
     else:
         print 'Length mismatch OR string input is empty'
         return False
@@ -24,7 +24,26 @@ rotated_strings = ['erbottlewat', 'BCDA', 'ACDB', 'ABCDE', ""]
 
 
 for s1, s2 in zip(original_strings, rotated_strings):
-    print 's1: ' + s1
-    print 's2: ' + s2
-
     print(isSubstring(s1, s2))
+
+
+"""
+EXPLANATION:
+If we imagine that s2 is a rotation of s1, then we can ask what the rotation point is. 
+
+FOR EXAMPLE, if you rotate WATERBOTTLE after WAT, you get ERBOTTLEWAT. 
+
+In a rotation, we cut s1 into two parts, x and y and rearrange them to get s2.
+
+s1 = xy = [wat][erbottle]
+
+x = wat
+y = erbottle
+
+s2 = yx = [erbottle][wat]
+
+So, we need to check if there's a way to split s1 into x and y such that xy = s1 and yx = s2.
+Regardless of where the division between x and y is, we can see that yx will always be a substring of xyxy. 
+That is, s2 will always be a substring of s1s1.
+
+"""
