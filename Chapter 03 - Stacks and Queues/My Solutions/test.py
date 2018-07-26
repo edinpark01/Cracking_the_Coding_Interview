b045008c57e5a3929418a3fc9fddf7d784d9367c
Braulio Tonaco <edu.tonaco@gmail.com>
2018-07-25 23:20:28 -0400

    Finished exercise 3.3

---
+++ Chapter 03 - Stacks and Queues/My Solutions/test.py
@@ -0,0 +1,64 @@
+from exercise_3_3 import SetOfStacks
+
+stack = SetOfStacks()
+
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+stack.push(5)
+stack.push(50)
+stack.push(51)
+stack.push(52)
+stack.push(53)
+stack.push(54)
+stack.push(55)
+stack.push(56)
+
+print stack.stacks
+
+print stack.popAt(2)
+
+print stack.stacks
+
+print stack.popAt(1)
+
+print stack.stacks
+print stack.popAt(1)
+
+print stack.stacks
+print stack.popAt(1)
+
+print stack.stacks
+print stack.popAt(1)
+
+print stack.stacks
+print stack.popAt(1)
+
+print stack.stacks
+print stack.popAt(1)
+
+print stack.stacks
+
+print stack.pop()
+
+print stack.stacks
+
+print stack.popAt(0)
+
+print stack.stacks
+
+print stack.popAt(0)
+
+print stack.stacks
+
+print stack.popAt(0)
+
+print stack.stacks
+print stack.popAt(0)
+
+print stack.stacks
+
+stack.pop()
+print stack.stacks
+print stack.popAt(0)
